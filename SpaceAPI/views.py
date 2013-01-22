8731614c774f35c498252077ea826a6f849701c0
Iris <iris@farsetlabs.org.uk>
2013-01-22 01:57:06 +0000

    gah

--- SpaceAPI/views.py
+++ SpaceAPI/views.py
@@ -96,6 +96,7 @@ def api_open():
         config=json.load(open(JSON_FILE,'r'))
         state = request.args.get('state', None)
         if state is None:
+	    app.logger.error("State is not given; reading from input")
 	    state = digital_input_mask(BIGREDINPUT)
         app.logger.debug(request)
         state = (str(state) == "True") #Convert unicode true/false to bool
